55f83d859cb13c9650b6a8ed08ccc6338e9e8192
Propulse Agency <qwidermesmoudi2024@gmail.com>
2025-02-24 19:00:40 +0100

    set up ticketer-marketplace

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import autoCert from "anchor-pki/auto-cert/integrations/next";
 import type { NextConfig } from "next";
+
+const withAutoCert = autoCert({
+  enabledEnv: "development",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withAutoCert(nextConfig);
